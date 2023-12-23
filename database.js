54d1a18857fdcc1a7333985130ce38ff14eba3f3
ayush kumar <ayushk8295@gmail.com>
2023-12-23 23:33:35 +0530

    fiveth change

--- database.js
+++ database.js
@@ -2,15 +2,17 @@ import mysql from 'mysql2'
 import dotenv from 'dotenv'
 dotenv.config()
 
+const port = process.env.PORT;
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
+
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 }).promise()
 
 export async function UserInput(Name, Email,Message) {
-    const [result] = await pool.query(`
+    await pool.query(`
     INSERT INTO userDetails (Name, Email, Message)
     VALUES (?, ?,?)
     `, [Name, Email,Message])
--- index.js
+++ index.js
@@ -8,8 +8,7 @@ import {UserInput} from './database.js'
 
 
 const app = express();
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT;
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 

447cbabf7785d498570dad97cb1f170fd230cd86
ayush kumar <ayushk8295@gmail.com>
2023-12-24 19:54:32 +0530

    fiveth2 change

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ res.sendFile(__dirname + "/public/index.html");
 
 app.post("/submit", async(req, res) => {
   const {name, email, message} =req.body;
-  await UserInput(name, email,message);
-  res.sendFile("https://landingpg1.onrender.com/public/thankyouPage.html");
+  // await UserInput(name, email,message);
+  res.sendFile(__dirname + "/public/thankyouPage.html");
 });
 app.post("/back", async(req, res) => {
   res.redirect("/");
